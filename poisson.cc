89063e68e51a7d3bc5107ef79d9d74d3c281ea8a
BenjaminLe99 <benjaminle21@hotmail.com>
2024-11-06 17:01:57 +0100

    Task c) almost

--- poisson.cc
+++ poisson.cc
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 
 double poisson(double mu, int k) {
-    return 0;
+    double pow(double x, double k);
+    double exp(double x);
+    double tgamma(double x);
+    double poisson_out = 1/tgamma(k+1) * pow(mu, k) * exp(-mu);
+    return poisson_out;
 }
 
 int main() {
@@ -12,9 +17,20 @@ int main() {
 
 
     ifstream fin("datensumme.txt");
+    ofstream outputfile("hist.txt");
+    ofstream histout("histpoi.txt");
     int n_i;
+    vector<int> zaehler(11);
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler[n_i] += 1;
+    }
+    for (unsigned int k = 0; k < zaehler.size(); ++k) {
+        cout << k << ":" << zaehler [k] << endl;
+        outputfile << k << " " << zaehler[k] << endl;
+        histout << k << " " << zaehler[k] << " " << 234 * poisson(3.11538, k) << endl;
     }
     fin.close();
+    outputfile.close();
+    histout.close();
 }
